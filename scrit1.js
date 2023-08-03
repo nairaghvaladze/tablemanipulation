071512fc862ed895b3666c8636134978477c1aed
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-08-03 20:10:25 +0400

    Add files via upload

---
+++ scrit1.js
@@ -0,0 +1,27 @@
+
+   
+   
+      var td =document.getElementsByTagName("td");
+    for (var i=0; i<td.length; i++){
+    
+        if(td[i].innerHTML % 2 == 0){
+            td[i].onmouseover=function(){
+                this.style.backgroundColor="green";
+               }
+             td[i].onmouseout=function(){
+                this.style.backgroundColor="white";
+               }
+          }else(td[i].innerHTML % 2 == 1) 
+        {
+            td[i].onmouseover=function(){
+                this.style.backgroundColor="red";
+               }
+             td[i].onmouseout=function(){
+                this.style.backgroundColor="white";
+               }
+
+        }
+        console.log(td[i].innerHTML)
+    }
+   
+   
